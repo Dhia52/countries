6242347889e8b2f4c5513e2cf818eb9e89d5a506
Dhia <dhia.bani@gmail.com>
2021-08-26 16:53:31 +0200

    update: changed continents' svg directory

--- src/data/Continents.ts
+++ src/data/Continents.ts
@@ -1,9 +1,9 @@
 import { Continent } from '../types/Continent'
 
 export const continentsArray: Continent[] = [
-    { name: 'Africa', img: '/src/assets/images/continents/africa.svg', code: 'AF' },
-    { name: 'Americas', img: '/src/assets/images/continents/americas.svg', code: 'NASA' },
-    { name: 'Asia', img: '/src/assets/images/continents/asia.svg', code: 'AS' },
-    { name: 'Europe', img: '/src/assets/images/continents/europe.svg', code: 'EU' },
-    { name: 'Oceania', img: '/src/assets/images/continents/oceania.svg', code: 'OC' }
+    { name: 'Africa', img: '/images/continents/africa.svg', code: 'AF' },
+    { name: 'Americas', img: '/images/continents/americas.svg', code: 'NASA' },
+    { name: 'Asia', img: '/images/continents/asia.svg', code: 'AS' },
+    { name: 'Europe', img: '/images/continents/europe.svg', code: 'EU' },
+    { name: 'Oceania', img: '/images/continents/oceania.svg', code: 'OC' }
 ]
